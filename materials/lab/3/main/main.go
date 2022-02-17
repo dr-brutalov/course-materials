d3524136ac48c0fe398b9150a866fa6c3311bf1b
clay-carper <ccarper2@uwyo.edu>
2022-02-17 10:28:54 -0700

    Auto-formatter changes

--- materials/lab/3/main/main.go
+++ materials/lab/3/main/main.go
@@ -6,10 +6,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
-	"encoding/json"
 	"shodan/shodan"
 )
 
@@ -35,20 +35,18 @@ func main() {
 
 	fmt.Printf("Host Data Dump\n")
 	for _, host := range hostSearch.Matches {
-		fmt.Println("==== start ",host.IPString,"====")
-		h,_ := json.Marshal(host)
+		fmt.Println("==== start ", host.IPString, "====")
+		h, _ := json.Marshal(host)
 		fmt.Println(string(h))
-		fmt.Println("==== end ",host.IPString,"====")
+		fmt.Println("==== end ", host.IPString, "====")
 		//fmt.Println("Press the Enter Key to continue.")
 		//fmt.Scanln()
 	}
-
 
 	fmt.Printf("IP, Port\n")
 
 	for _, host := range hostSearch.Matches {
 		fmt.Printf("%s, %d\n", host.IPString, host.Port)
 	}
-
 
 }
